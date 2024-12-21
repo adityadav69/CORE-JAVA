65b26788ac641746c21c268f1c5a38d8eacbf0a8
Aditya yadav <adityayadaw42@gmail.com>
2024-12-21 18:38:49 +0530

    basic cheques + comment

--- Hello.java
+++ Hello.java
@@ -1,5 +1,10 @@
 public class Hello{
     public static void main(String args[]){
         System.out.println("Hello world");
+
+        // Single line comment in java
+        /*This
+         is multiline comment in java
+         */
     }
 }
