ac32179fdc90c53e1052ef24dfc00d31db984c9f
Aditya yadav <adityayadaw42@gmail.com>
2024-12-23 18:24:46 +0530

    operator

--- operator.java
+++ operator.java
@@ -2,14 +2,14 @@ public class operator {
     public static void main(String[] args) {
         int a=4;
         int b=5;
-        // System.out.println(a+b);
-        // System.out.println(a-b);
-        // System.out.println(a*b);
-        // System.out.println(a/b);
-        // System.out.println(a%b);
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
          
-        Byte b1=10;
-        Byte b2=5;
+        // Byte b1=10;
+        // Byte b2=5;
         // Byte byteSum=b1+b2; 
         //Above will throw error because in b1 and b2 value may change in future and it may go out of scope of byte
         // System.out.println("byteSum = "+ byteSum);
@@ -17,19 +17,23 @@ public class operator {
       
         int x=10;
         int x1=++x;
-        // System.out.println(x1);
+        System.out.println(x1);
 
         int y=12;
         int y1=y++;
-        // System.out.println(y);
+        System.out.println(y);
+        System.out.println(y1);
 
         int xx=10;
         int xx1=--xx;
         System.out.println(xx);
+        System.out.println(xx1);
+
 
         int yy=12;
         int yy1=yy--;
         System.out.println(yy1);
+        System.out.println(yy);
 
         
 
