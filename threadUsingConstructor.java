3a5362a58edab3e45d1d953e75a653723c8aec2d
Aditya yadav <adityayadaw42@gmail.com>
2025-01-01 16:15:17 +0530

    thread using constructor

---
+++ threadUsingConstructor.java
@@ -0,0 +1,36 @@
+class Thread1 extends Thread{
+    public void run(){
+        int i=0;
+        while (i!=100) {
+            System.out.println("Thread 1 is running");
+            i++;
+        }
+    }
+    Thread1(String name){
+        super(name);
+    }
+}
+class Thread2 extends Thread{
+    public void run(){
+        int i=0;
+        while (i!=100) {
+            System.out.println("Thread 2 is running");
+            i++;
+        }
+    }
+    Thread2(String name){
+        super(name);
+    }
+}
+public class threadUsingConstructor {
+    public static void main(String[] args) {
+        Thread1 t1=new Thread1("Aditya");
+        Thread2 t2=new Thread2("Aditya yadav");
+        System.out.println("Name of thread 1 is "+t1.getName());
+        System.out.println("Name of thread 2 is "+t2.getName());
+        System.out.println("Name of thread 1 is "+t1.getId());
+        System.out.println("Name of thread 2 is "+t2.getId());
+
+        t1.start();
+    }
+}
